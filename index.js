44b750707ae9422cefba219eefb508d626760af4
Paco1994 <i32rodrf@uco.es>
2018-08-25 00:36:03 +0200

    Refactor the Main file

--- index.js
+++ index.js
@@ -1,40 +1,14 @@
 'use strict';
-
-require('dotenv').config();
-
+require('dotenv').config({path: __dirname + '/.env'});
 const Alexa = require('alexa-sdk');
 
-
 const myHandlers = require('./handlers');
-const messages = require('./messages.json');
 
 const APP_ID = process.env.APP_ID;
 
-const handlers = {
-    'LaunchRequest': function () {
-      this.emit('Greeting');
-    },
-    'Greeting': myHandlers.greeting,
-    'AMAZON.HelpIntent': function () {
-        const speechOutput = messages.help;
-        const reprompt = messages.help_reprompt;
-
-        this.response.speak(speechOutput).listen(reprompt);
-        this.emit(':responseReady');
-    },
-    'AMAZON.CancelIntent': function () {
-        this.response.speak(messages.stop);
-        this.emit(':responseReady');
-    },
-    'AMAZON.StopIntent': function () {
-        this.response.speak(messages.stop);
-        this.emit(':responseReady');
-    },
-};
-
 exports.handler = (event, context, callback) => {
   const alexa = Alexa.handler(event, context, callback);
-  alexa.APP_ID = APP_ID;
-  alexa.registerHandlers(handlers);
+  alexa.appId = APP_ID;
+  alexa.registerHandlers(myHandlers.newSessionHandler, myHandlers.askForCouponHandler);
   alexa.execute();
 };
